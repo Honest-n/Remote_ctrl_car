6745d10f3796bcd723c8694282aea73c09b48028
Honest-n <79773965+Honest-n@users.noreply.github.com>
2023-05-10 09:46:11 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,70 +1,57 @@
-def on_button_pressed_a():
-    global Speed, Rotation2
-    Speed = 0
-    Rotation2 = -100
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def Brake():
-    global Speed
-    if Speed >= 10:
-        Brakelights.set_brightness(20)
-        Speed += -10
-        Brakelights.set_brightness(10)
-    else:
-        Brakelights.set_brightness(20)
-        Brakelights.set_brightness(10)
-
-def on_button_pressed_ab():
-    global Speed, Rotation2
+def Reset():
+    global Reverse, Speed, Rotation2, strip, Brakelights, Headlights
+    Reverse = False
     Speed = 0
     Rotation2 = 0
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    global Speed, Rotation2
-    Speed = 0
-    Rotation2 = 100
-input.on_button_pressed(Button.B, on_button_pressed_b)
+    strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
+    Brakelights = strip.range(5, 2)
+    Headlights = strip.range(0, 2)
+    strip.set_brightness(10)
+    Headlights.show_color(neopixel.colors(NeoPixelColors.WHITE))
+    Brakelights.show_color(neopixel.colors(NeoPixelColors.RED))
 
 def on_mes_dpad_controller_id_microbit_evt():
-    global Speed, Rotation2
-    if control.event_value() == EventBusValue.MES_DPAD_BUTTON_D_DOWN:
-        music.play_sound_effect(music.builtin_sound_effect(soundExpression.hello),
-            SoundExpressionPlayMode.UNTIL_DONE)
+    global Speed, Reverse, Rotation2
     if control.event_value() == EventBusValue.MES_DPAD_BUTTON_1_DOWN:
-        Speed += 10
-    if control.event_value() == EventBusValue.MES_DPAD_BUTTON_2_DOWN:
-        Brake()
-    if control.event_value() == EventBusValue.MES_DPAD_BUTTON_3_DOWN:
-        Rotation2 += -10
-    if control.event_value() == EventBusValue.MES_DPAD_BUTTON_4_DOWN:
-        Rotation2 += 10
-    if control.event_value() == EventBusValue.MES_DPAD_BUTTON_A_DOWN:
-        Headlights.set_brightness(20)
-        Headlights.set_brightness(10)
+        if Reverse == True:
+            Speed += -10
+        else:
+            Speed += 10
+    else:
+        if control.event_value() == EventBusValue.MES_DPAD_BUTTON_2_DOWN:
+            if Speed != 0:
+                if Reverse == True:
+                    Speed += 10
+                else:
+                    Speed += 0 - 10
+            elif Reverse == False:
+                Reverse = True
+                Brakelights.show_color(neopixel.colors(NeoPixelColors.WHITE))
+            else:
+                Reverse = False
+                Brakelights.show_color(neopixel.colors(NeoPixelColors.RED))
+        else:
+            if control.event_value() == EventBusValue.MES_DPAD_BUTTON_3_DOWN:
+                Rotation2 += -10
+            else:
+                if control.event_value() == EventBusValue.MES_DPAD_BUTTON_4_DOWN:
+                    Rotation2 += 10
+                else:
+                    if control.event_value() == EventBusValue.MES_DPAD_BUTTON_A_DOWN:
+                        pass
+                    else:
+                        if control.event_value() == EventBusValue.MES_DPAD_BUTTON_D_DOWN:
+                            Reset()
+    servos.P2.run(Rotation2 - Speed)
+    servos.P1.run(Rotation2 + Speed)
 control.on_event(EventBusSource.MES_DPAD_CONTROLLER_ID,
     EventBusValue.MICROBIT_EVT_ANY,
     on_mes_dpad_controller_id_microbit_evt)
 
-Rotation2 = 0
-Speed = 0
-Brakelights: neopixel.Strip = None
 Headlights: neopixel.Strip = None
-strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
-Headlights = strip.range(8, 2)
-Brakelights = strip.range(3, 2)
-Brakelights.set_brightness(10)
-Headlights.set_brightness(10)
-Headlights.show_color(neopixel.colors(NeoPixelColors.WHITE))
-Brakelights.show_color(neopixel.colors(NeoPixelColors.RED))
-Speed = 0
+Brakelights: neopixel.Strip = None
+strip: neopixel.Strip = None
 Rotation2 = 0
-
-def on_forever():
-    servos.P1.run(Rotation2 + Speed)
-    servos.P2.run(Rotation2 - Speed)
-basic.forever(on_forever)
-
-def on_forever2():
-    pass
-basic.forever(on_forever2)
+Speed = 0
+Reverse = False
+Reset()
--- main.ts
+++ main.ts
@@ -1,61 +1,69 @@
-input.onButtonPressed(Button.A, function () {
-    Speed = 0
-    Rotation2 = -100
-})
-function Brake () {
-    if (Speed >= 10) {
-        Speed += -10
-    } else {
-    	
-    }
-}
-input.onButtonPressed(Button.AB, function () {
+function Reset () {
+    Reverse = false
     Speed = 0
     Rotation2 = 0
-})
-input.onButtonPressed(Button.B, function () {
-    Speed = 0
-    Rotation2 = 100
-})
-control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
-        Speed = 0
+    strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
+    Brakelights = strip.range(5, 2)
+    Headlights = strip.range(0, 2)
+    strip.setBrightness(10)
+    Headlights.showColor(neopixel.colors(NeoPixelColors.White))
+    Brakelights.showColor(neopixel.colors(NeoPixelColors.Red))
+}
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, function () {
+    while (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN && Speed < 100) {
+        if (Reverse == true) {
+            Speed += -10
+        } else {
+            Speed += 10
+        }
+        basic.pause(10)
     }
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
-        Speed += 10
+    while (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        if (Speed != 0) {
+            if (Reverse == true) {
+                Speed += 10
+            } else {
+                Speed += -10
+            }
+        } else if (Reverse == false) {
+            Reverse = true
+            Brakelights.showColor(neopixel.colors(NeoPixelColors.White))
+            break;
+        } else {
+            Reverse = false
+            Brakelights.showColor(neopixel.colors(NeoPixelColors.Red))
+            break;
+        }
+        basic.pause(10)
     }
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
-        Brake()
-    }
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+    while (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN && Rotation2 > -100) {
         Rotation2 += -10
+        basic.pause(10)
     }
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+    while (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN && Rotation2 < 100) {
         Rotation2 += 10
+        basic.pause(10)
     }
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
-        Headlights.setBrightness(20)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
-        basic.pause(100)
-        Headlights.setBrightness(10)
+        Reset()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+        basic.pause(50)
     }
 })
+let Headlights: neopixel.Strip = null
+let Brakelights: neopixel.Strip = null
+let strip: neopixel.Strip = null
 let Rotation2 = 0
 let Speed = 0
-let Headlights: neopixel.Strip = null
-let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
-let Brakelights = strip.range(5, 2)
-Headlights = strip.range(0, 2)
-Brakelights.setBrightness(10)
-Headlights.setBrightness(10)
-Headlights.showColor(neopixel.colors(NeoPixelColors.White))
-Brakelights.showColor(neopixel.colors(NeoPixelColors.Red))
-Speed = 0
-Rotation2 = 0
+let Reverse = false
+Reset()
 basic.forever(function () {
-	
+    let R_speed = 0
+    let L_speed = 0
+    servos.P1.run(L_speed)
+    servos.P2.run(R_speed)
 })
-basic.forever(function () {
-    servos.P1.run(Rotation2 + Speed)
-    servos.P2.run(Rotation2 - Speed)
+control.inBackground(function () {
+    control.waitForEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_DOWN)
 })
